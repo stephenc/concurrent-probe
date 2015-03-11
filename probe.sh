3926bd48ca15efd14e3bb77a012bba48da998ef9
Stephen Connolly <stephen.alan.connolly@gmail.com>
2015-03-11 12:13:23 +0000

    probe.sh

---
+++ probe.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+if [ `uname` == "Darwin" ] ; then
+  alias md5sum='md5 -q'
+else
+  alias md5sum='md5sum --quiet'
+fi
+if [ "A$1" == "A" ] ; then
+  echo "You must supply the name of the file to use as a probe"
+  exit 2
+fi
+PROBE="$1"
+DELTA="$(mktemp "$PROBE.XXXXXX")"
+if [ "A$2" == "A" ] ; then
+  WORK=60
+else
+  WORK=$2
+fi
+echo "Starting probe $PROBE [pid=$$]"
+echo "[$(date),pid=$$] Starting probe $PROBE" | tee "$DELTA" > "$PROBE"
+for n in $(seq 1 $WORK) ; do
+  printf "."
+  echo "[$(date),pid=$$] Work $n: $RANDOM" | tee -a "$DELTA" >> "$PROBE"
+  sleep 1
+done
+echo " done"
+echo "[$(date),pid=$$] Finished probe $PROBE: $RANDOM" | tee -a "$DELTA" >> "$PROBE"
+PROBE_MD5=$(md5sum "$PROBE")
+DELTA_MD5=$(md5sum "$DELTA")
+echo "Probe $PROBE hash found: $PROBE_MD5 expecting: $DELTA_MD5"
+
+rm -f "$DELTA"
+if [ "$PROBE_MD5" == "$DELTA_MD5" ] ; then
+  echo "SUCCESS"
+else
+  cat "$PROBE" | sed -ne "s/^/    /p;"
+  echo "FAILURE"
+  exit 1
+fi
